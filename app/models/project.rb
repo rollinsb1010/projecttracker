616f2676b081f03d3049d40b3d029a272dbe926e
Brian Rollins <rollinsb@austin.rr.com>
2012-11-09 17:31:28 -0600

    bootstrap follies

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
    attr_accessible :project_id, :project_name, :due_date, :components, :notes, :client_name, :project_manager_assigned,
        :account_manager_assigned, :medium, :stage, :percentage_complete, :description
+       
+       belongs_to :admin
 end
